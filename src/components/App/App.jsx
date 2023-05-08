f81ba5d86df73560ce0196c10d8dc8908579f577
Ihor849 <333marchelo333@gmail.com>
2023-05-08 11:46:27 +0300

    виправля пошук

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -20,12 +20,11 @@ export class App extends Component {
     error: '',
   };
   handleSubmit = async searchQuery => {
-    const { page } = this.state;
     this.setState({ query: searchQuery, page: 1 });
 
     try {
       this.setState({ status: 'pending' });
-      const { hits, totalHits } = await fetchImages(searchQuery, page);
+      const { hits, totalHits } = await fetchImages(searchQuery, 1);
       // console.log(hits, totalHits);
       if (hits.length < 1) {
         this.setState({ status: 'idle', totalHits: 0 });
